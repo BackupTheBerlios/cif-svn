49342aa15a1c1bc9a4c2c616106a355d92739aff
cifactorybuild <cifactorybuild@5c17bd53-ed07-0410-a614-d837f92bc533>
2005-12-19 11:57:54 +0000

    CCNET build 1.0.0.7

---
+++ Tags/1.0.0.7/Product/Production/Foundation/Default/Product/ProjectInfo.vb
@@ -0,0 +1,7 @@
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+<Assembly: AssemblyCompany("NGIT")> 
+<Assembly: AssemblyProduct("%ProjectName%")> 
+<Assembly: AssemblyInformationalVersion("0.0.0.0")> 
