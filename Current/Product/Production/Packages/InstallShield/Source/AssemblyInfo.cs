d969ef01c99f5fb08f2fc514ce22455609d9a195
cifactorybuild <cifactorybuild@5c17bd53-ed07-0410-a614-d837f92bc533>
2005-12-20 18:25:55 +0000

    Modified by build script in build 1.0.0.3

--- Current/Product/Production/Packages/InstallShield/Source/AssemblyInfo.cs
+++ Current/Product/Production/Packages/InstallShield/Source/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,4 +6,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("nAnt tasks for compiling InstallShield projects")]
 [assembly: ComVisible(false)]
 [assembly: Guid("b8232f9c-2080-4e1c-adcb-e276c08ff0dd")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
