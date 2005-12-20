08c3d22f1eedc4fd1cb30009bd7b7aad208c2d49
cifactorybuild <cifactorybuild@5c17bd53-ed07-0410-a614-d837f92bc533>
2005-12-20 14:52:30 +0000

    Modified by build script in build 1.0.0.2

--- Current/Product/ProjectInfo.cs
+++ Current/Product/ProjectInfo.cs
@@ -4,4 +4,4 @@ Imports System.Runtime.InteropServices
 
 <Assembly: AssemblyCompany("NGIT")> 
 <Assembly: AssemblyProduct("TestProject")> 
-<Assembly: AssemblyInformationalVersion("1.0.0.1")> 
+<Assembly: AssemblyInformationalVersion("1.0.0.2")> 
