54d90a653a79c9f2ff57ed09d849fbe237a5b1ca
cifactorybuild <cifactorybuild@5c17bd53-ed07-0410-a614-d837f92bc533>
2005-12-20 18:25:45 +0000

    Modified by build script in build 1.0.0.3

--- Current/Product/Production/Common/VSS.Tasks/AssemblyInfo.vb
+++ Current/Product/Production/Common/VSS.Tasks/AssemblyInfo.vb
@@ -6,4 +6,4 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyDescription("nAnt tasks for managing Visual Source Safe")> 
 <Assembly: CLSCompliant(True)> 
 <Assembly: Guid("88F28363-D5CD-49F2-B563-3038357DDA80")> 
-<Assembly: AssemblyVersion("1.0.0.1")> 
+<Assembly: AssemblyVersion("1.0.0.2")> 
